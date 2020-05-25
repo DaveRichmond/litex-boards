c30d483e39b36ad258b4f9ab6dedc09497bc6402
David Richmond <dave@prstat.org>
2020-05-25 14:58:58 +1000

    QMTech Artix7 DDR3 Board

---
+++ litex_boards/platforms/qmtech_a7ddr3.py
@@ -0,0 +1,95 @@
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform, VivadoProgrammer
+
+_io = [
+    ("cpu_reset", 0, Pins("K5"), IOStandard("LVCMOS33")),
+    ("clk50", 0, Pins("N11"), IOStandard("LVCMOS33")),
+
+    ("spiflash4x", 0, 
+        Subsignal("cs_n", Pins("L12")),
+        Subsignal("clk", Pins("E8")),
+        Subsignal("dq", Pins("J13", "J14", "K15", "K16")),
+        IOStandard("LVCMOS33")),
+    ("spiflash", 0,
+        Subsignal("cs_n", Pins("L12")),
+        Subsignal("clk", Pins("E8")),
+        Subsignal("mosi", Pins("J13")),
+        Subsignal("miso", Pins("J14")),
+        Subsignal("wp", Pins("K15")),
+        Subsignal("hold", Pins("K16")),
+        IOStandard("LVCMOS33")),
+
+    ("ddram", 0, 
+        Subsignal("a", Pins(
+            "B14 C8 A14 C14 C9 B10 D9 A12",
+            "D8 A13 B12 A9 A8 B11"),
+            IOStandard("SSTL135")),
+        Subsignal("ba", Pins("C16 A15 B15"), IOStandard("SSTL135")),
+        Subsignal("ras_n", Pins("B16"), IOStandard("SSTL135")),
+        Subsignal("cas_n", Pins("C11"), IOStandard("SSTL135")),
+        Subsignal("we_n", Pins("C12"), IOStandard("SSTL135")),
+        #Subsignal("cs_n", Pins(""), IOStandard("SSTL135")),
+        Subsignal("dm", Pins("F12 H11"), IOStandard("SSTL135")),
+        Subsignal("dq", Pins(
+            "F15 F13 E16 D11 E12 E13 D16 E11",
+            "G12 J16 G16 J15 H14 H12 H16 H13"),
+            IOStandard("SSTL135"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("dqs_p", Pins("D14 G14"),
+            IOStandard("DIFF_SSTL135"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("dqs_n", Pins("D15 F14"),
+            IOStandard("DIFF_SSTL135"),
+            Misc("IN_TERM=UNTUNED_SPLIT_40")),
+        Subsignal("clk_p", Pins("B9"), IOStandard("DIFF_SSTL135")),
+        Subsignal("clk_n", Pins("A10"), IOStandard("DIFF_SSTL135")),
+        Subsignal("cke", Pins("D13"), IOStandard("SSTL135")),
+        Subsignal("odt", Pins("C13"), IOStandard("SSTL135")),
+        Subsignal("reset_n", Pins("E15"), IOStandard("SSTL135")),
+        Misc("SLEW=FAST"),
+    ),
+
+    ("led", 0, Pins("E6"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("R6"), IOStandard("LVCMOS33")),
+    ("user_led", 1, Pins("T5"), IOStandard("LVCMOS33")),
+    ("user_led", 2, Pins("R7"), IOStandard("LVCMOS33")),
+    ("user_led", 3, Pins("T7"), IOStandard("LVCMOS33")),
+    ("user_led", 4, Pins("R8"), IOStandard("LVCMOS33")),
+    ("user_btn", 0, Pins("B7"), IOStandard("LVCMOS33")),
+    ("user_btn", 1, Pins("M6"), IOStandard("LVCMOS33")),
+    ("user_btn", 2, Pins("N6"), IOStandard("LVCMOS33")),
+    ("user_btn", 3, Pins("R5"), IOStandard("LVCMOS33")),
+    ("user_btn", 4, Pins("P6"), IOStandard("LVCMOS33")),
+
+    ("sdcard", 0,
+            Subsignal("data", Pins("B5 B6 J4 J5")),
+            Subsignal("cmd", Pins("K5")),
+            Subsignal("clk", Pins("E6")),
+            Subsignal("cd", Pins("A7")),
+            IOStandard("LVCMOS33"), Misc("SLEW=FAST")),
+
+    ("serial", 0, 
+            Subsignal("tx", Pins("T14")),
+            Subsignal("rx", Pins("T15")),
+            IOStandard("LVCMOS33")),
+
+]
+
+class Platform(XilinxPlatform):
+    default_clk_name = "clk50"
+    default_clk_period = 1e9/50e6
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xc7a35tftg256-1", _io, toolchain="vivado")
+        self.toolchain.bitstream_commands = \
+            ["set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]"]
+        self.toolchain.additional_commands = \
+            ["write_cfgmem -force -format bin -interface spix4 -size 16 "
+             "-loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.bit"]
+        self.add_platform_command("set_property INTERNAL_VREF 0.675 [get_iobanks 15]")
+
+    def do_finalize(self, fragment):
+        XilinxPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk50", loose=True), 1e9/50e6)
+
+    def create_programmer(self):
+        return VivadoProgrammer()
---
+++ litex_boards/targets/qmtech_a7ddr3.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+
+from migen import *
+from litex_boards.platforms import qmtech_a7ddr3
+from litex.build.xilinx.vivado import vivado_build_args, vivado_build_argdict
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc_sdram import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+from litedram.modules import MT41K128M16
+from litedram.phy import s7ddrphy
+
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys       = ClockDomain()
+        self.clock_domains.cd_sys2x     = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys4x     = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
+        self.clock_domains.cd_clk200    = ClockDomain()
+        self.clock_domains.cd_eth       = ClockDomain()
+
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
+        self.comb += pll.reset.eq(~platform.request("cpu_reset"))
+        pll.register_clkin(platform.request("clk50"), 50e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+        pll.create_clkout(self.cd_sys2x, 2*sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x, 4*sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
+        pll.create_clkout(self.cd_clk200, 200e6)
+        pll.create_clkout(self.cd_eth, 25e6)
+
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_clk200)
+        #self.comb += platform.request("eth_ref_clk").eq(self.cd_eth.clk)
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=int(100e6), with_ethernet=False, **kwargs):
+        platform = qmtech_a7ddr3.Platform()
+        SoCCore.__init__(self, platform, clk_freq=sys_clk_freq, **kwargs)
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+        if not self.integrated_main_ram_size:
+            self.submodules.ddrphy = s7ddrphy.A7DDRPHY(
+                    platform.request("ddram"),
+                    memtype = "DDR3",
+                    nphases = 4,
+                    sys_clk_freq = sys_clk_freq,
+                    interface_type = "MEMORY")
+            self.add_csr("ddrphy")
+            self.add_sdram("sdram",
+                    phy = self.ddrphy,
+                    module = MT41K128M16(sys_clk_freq, "1:4"),
+                    origin = self.mem_map["main_ram"],
+                    size = kwargs.get("max_sdram_size", 0x40000000),
+                    l2_cache_size = kwargs.get("l2_size", 8192),
+                    l2_cache_min_data_width = kwargs.get("min_l2_data_width", 128),
+                    l2_cache_reverse = True)
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on QMTech Artix 7 DDR3")
+    parser.add_argument("--build", action="store_true", help="build bitstream")
+    parser.add_argument("--load", action="store_true", help="load bitstream")
+    builder_args(parser)
+    soc_sdram_args(parser)
+    vivado_build_args(parser)
+    parser.add_argument("--with-ethernet", action="store_true", help="Enable ethernet")
+    args = parser.parse_args()
+
+    soc = BaseSoC(with_ethernet=args.with_ethernet, **soc_sdram_argdict(args))
+    builder = Builder(soc, **builder_argdict(args))
+    builder.build(**vivado_build_argdict(args), run=args.build)
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+
+if __name__ == "__main__":
+    main()
+
